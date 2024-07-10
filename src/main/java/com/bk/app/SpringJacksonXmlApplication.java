ab65fac91ef26f91d90fd791833b5860d05e8dc0
mmsaleh_github <salman1m22@gmail.com>
2024-07-11 00:17:44 +0600

    com.fasterxml.jackson.dataformat added and project started

---
+++ src/main/java/com/bk/app/SpringJacksonXmlApplication.java
@@ -0,0 +1,12 @@
+package com.bk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJacksonXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJacksonXmlApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bk/app/SpringJacksonXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bk.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJacksonXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
